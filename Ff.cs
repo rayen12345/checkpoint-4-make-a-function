90c9c1ea3982483a28077ab2c1422b12e6a8127c
rayen12345 <54586143+rayen12345@users.noreply.github.com>
2019-08-30 22:01:34 +0100

    Add files via upload

---
+++ Ff.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ff : MonoBehaviour
+{
+    void sum(int a, int b, int c)
+       { int x = a + b + c;
+    Debug.Log(x) ;
+}
+    void difference(int a, int b)
+    { int d = a - b;
+        Debug.Log(d);
+    }
+    void product (int a, int b)
+    { int p = a * b;
+        Debug.Log(p);
+    }
+
+
+
+    void Start()
+{
+        sum(12, 5, 8);
+        difference(10, 7);
+        product(5, 9);
+
+
+    }
+
+    
+}
